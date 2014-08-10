816b42a136762218182ec65a40117300210fe7cd
Ken Chan <kchan923@gmail.com>
2014-08-10 09:43:46 -0400

    resolving issues

--- js/ListController.js
+++ js/ListController.js
@@ -4,25 +4,39 @@
         console.log("ListController.init()");
     },
 
-    setResults: function (addresses) {
+    setResults: function (addresses, isNearbyLabel) {
         ListController.clear();
 
         //  Display the table rows dynamically.
 
         var s = "";
+        var count = 0;
 
-        s += "<li><h5 id='list-results'>Showing Nearby Results</h5></li><hr/>"
+        if(addresses == null)
+        {
+            s += "<ul class='no-result-table'>";
+            s += "<li><h5 id='no-results'>No Results</h5><span id='clear-results'>Clear</span>";
+            s += "</li><hr/>";
+            s += "</ul>";
+        }
+        else
+        {
+            s += "<ul class='result-table'>";
+            s += "<li><h5 id='list-results'>Showing " + addresses.length + " Nearby Results</h5></li><hr/>";
 
-        addresses.forEach(function (addressEntry) {
-            s += "<li class='address-items'><strong>";
-            s += "<span class='glyphicon glyphicon-map-marker'></span>" 
-            s += addressEntry.fullAddress + ", ";
-            s += addressEntry.mailingNeighborhood + " ";
-            s += addressEntry.zipCode;
-            s += "</strong></li><hr/>";
-        });
+            addresses.forEach(function (addressEntry) {
+                s += "<li class='address-items'><strong>";
+                s += "<span class='glyphicon glyphicon-map-marker'></span>" 
+                s += addressEntry.fullAddress + ", ";
+                s += addressEntry.mailingNeighborhood + " ";
+                s += addressEntry.zipCode;
+                s += "</strong></li><hr/>";
+            });
 
-        d3.select(".result-table").html(s);
+            s += "</ul>";
+        }   
+
+        d3.select(".result-content").html(s);
 
         d3.selectAll("ul li.address-items").data(addresses).on("mouseover", function () {
             d3.select(this).style("background-color", "rgba(200, 200, 200, 0.5)").style("cursor", "pointer");
